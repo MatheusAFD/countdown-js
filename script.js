e8077dec117b76a8debf3fc04356193457d60a4a
Matheus Araújo <matheus.daf@outlook.com>
2022-06-14 16:50:37 -0300

    new render

--- script.js
+++ script.js
@@ -2,14 +2,11 @@ import Countdown from "./countdown.js";
 
 const happyNewYear = new Countdown("31 December 2022 23:59:59 GMT-0300");
 
-const days = document.querySelector(".days");
-const hours = document.querySelector(".hours");
-const minutes = document.querySelector(".minutes");
-const seconds = document.querySelector(".seconds");
-
 setInterval(() => {
-  days.innerText = happyNewYear.total.days;
-  hours.innerText = happyNewYear.total.hours;
-  minutes.innerText = happyNewYear.total.minutes;
-  seconds.innerText = happyNewYear.total.seconds;
+  days.textContent = happyNewYear.total.days;
+  hours.textContent = happyNewYear.total.hours;
+  minutes.textContent = happyNewYear.total.minutes;
+  seconds.textContent = happyNewYear.total.seconds;
 }, 1000);
+
+spank.textContent = "a";
